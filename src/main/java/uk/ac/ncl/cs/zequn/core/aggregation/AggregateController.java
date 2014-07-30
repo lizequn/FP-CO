e073f370eaf1566602b052dbc41e3e9c18272019
zequnli <lzq910123@gmail.com>
2014-07-30 18:16:15 +0100

    fix timer

--- src/main/java/uk/ac/ncl/cs/zequn/core/aggregation/AggregateController.java
+++ src/main/java/uk/ac/ncl/cs/zequn/core/aggregation/AggregateController.java
@@ -27,14 +27,18 @@ public class AggregateController {
     private final AtomicReference<Tuple> remoteOldTuple = new AtomicReference<Tuple>();
     private LinkedList<Index> index;
     private long totalCount = 0;
+    private OldTupleRequester requester;
+    private Aggregation aggregation;
     public AggregateController(final int aggregateID,final Aggregation aggregation, final int sliceTime, final int rangeTime,final OldTupleRequester requester){
         this.aggregateID = aggregateID;
+        this.aggregation = aggregation;
         this.inMemoryQueue = new InMemoryQueue();
         this.factory = new TupleFactory(aggregation);
         this.sliceTime = sliceTime;
         this.rangeTime = rangeTime;
         this.result = new Result();
         this.timer = new Timer();
+        this.requester = requester;
         this.timerTask = new TimerTask() {
             @Override
             public void run() {
@@ -75,6 +79,47 @@ public class AggregateController {
                 logger.info(totalCount+"");
             }
         };
+    }
+    class myTimerTask extends TimerTask{
+
+        @Override
+        public void run() {
+            //get new tuple
+            Tuple newTuple = factory.getTuple();
+            if(newTuple == null) newTuple = new Tuple();
+            inMemoryQueue.offer(newTuple);
+            totalCount++;
+            if(index.getLast().id == Config.id){
+                index.getLast().count++;
+            }else {
+                Index index1 = new Index();
+                index1.id = Config.id;
+                index1.count = 1;
+                index.offer(index1);
+            }
+            Tuple oldTuple = null;
+//                if(inMemoryQueue.size()>= sliceTime/rangeTime){
+//                    oldTuple = inMemoryQueue.get();//
+//                }
+            //need moving window
+
+            if(totalCount>=rangeTime/sliceTime){
+                if(index.getFirst().id == Config.id){
+                    oldTuple = inMemoryQueue.get();
+                    index.getFirst().count--;
+                }else {
+                    index.getFirst().count--;
+                    //remote get
+                    //todo
+                    oldTuple = remoteOldTuple.get();
+                    requester.requestNext(Config.id);
+                }
+                totalCount--;
+            }
+            aggregation.updateResult(result,oldTuple,newTuple);
+            logger.info(result.getRe()+"");
+            logger.info(totalCount+"");
+        }
     }
     public Tuple get(){
         logger.info("get Tuple");
@@ -122,6 +167,6 @@ public class AggregateController {
         logger.info("active");
         timer = new Timer();
         this.setIndex(in);
-        timer.scheduleAtFixedRate(timerTask,0,sliceTime);
+        timer.scheduleAtFixedRate(new myTimerTask(),0,sliceTime);
     }
 }
